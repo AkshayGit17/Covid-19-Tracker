9ef1a8ae33a6d9f82554c20d6d1912b01a8bd9d0
Your Name <you@example.com>
2021-02-16 00:31:24 +0530

    created header, fetched countries data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
+import { FormControl, MenuItem, Select } from '@material-ui/core';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      //get all the countries data when the component is loaded for the first time
+      const response = await fetch('https://disease.sh/v3/covid-19/countries');
+      const data = await response.json();
+
+      const trimmedData = data.map((country) => {
+        //excluding unwanted info in data
+        return {
+          name: country.country,
+          value: country.countryInfo.iso2,
+          id: country.countryInfo._id,
+        };
+      });
+
+      setCountries(trimmedData);
+    };
+
+    getCountriesData();
+  }, []);
+
   return (
-    <div className="App">
+    <div className='app'>
+      <div className='app__header'>
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className='app__dropdown'>
+          <Select variant='outlined'>
+            {countries.map((country, index) => {
+              return (
+                <MenuItem value={country.value} key={index}>
+                  {country.name}
+                </MenuItem>
+              );
+            })}
+          </Select>
+        </FormControl>
+      </div>
+
       {/* Title */}
       {/* Select input dropdown field */}
 
-      {/* Infoboxes */}
-      {/* Infoboxes */}
-      {/* Infoboxes */}
+      {/* Infobox */}
+      {/* Infobox */}
+      {/* Infobox */}
 
       {/* Map */}
 
